e6f3f2c38b43a521036043d2c779e5a317c451c6
Alan Mantoux <alan.mantoux@gmail.com>
2021-06-24 08:27:36 +0200

    Fix

--- build_and_deploy_linux.sh
+++ build_and_deploy_linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 clang -shared -fpic -Ofast src/frame_grabber.c -o ./libfgrabber.so -lavcodec -lavformat -lavutil
-cp ../libfgrabber.so /lib/x86_64-linux-gnu/.
+cp ./libfgrabber.so /lib/x86_64-linux-gnu/.
