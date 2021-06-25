d864021a57919f1d58e29e809f5096dc66d90376
Alan Mantoux <alan.mantoux@gmail.com>
2021-06-25 10:46:55 +0200

    Update build_mac_os.sh

--- build_mac_os.sh
+++ build_mac_os.sh
@@ -1,5 +1,4 @@
 
 #bash
-clang -shared -Ofast -fpic src/frame_grabber.c  -o ./libfgrabber.dylib -lavcodec -lavformat -lavutil
-clang -g src/main.c libfgrabber.dylib -lavutil -o ./out/test_frame_grabber
-mv libfgrabber.dylib ./out/.
+clang -shared -Ofast -fpic src/frame_grabber.c  -o .out/libfgrabber.dylib -lavcodec -lavformat -lavutil
+clang -g src/main.c ./out/libfgrabber.dylib -lavutil -o ./out/test_frame_grabber
