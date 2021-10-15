7e5ff08bf4e8127ecab1aae30c00a0b8824f42f0
Alan Mantoux <alan.mantoux@gmail.com>
2021-10-15 15:17:56 +0200

    Update build_and_deploy_linux.sh

--- build_and_deploy_linux.sh
+++ build_and_deploy_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-clang -shared -Ofast -I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin -fpic src/frame_grabber.c src/com_plato_utils_fgrabber_FrameGrabber.c -o ./out/libfgrabber.dylib -lavcodec -lavformat -lavutil
+clang -shared -Ofast -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -fpic src/frame_grabber.c src/com_plato_utils_fgrabber_FrameGrabber.c -o ./out/libfgrabber.dylib -lavcodec -lavformat -lavutil
 
 cp ./libfgrabber.so /lib/x86_64-linux-gnu/.
